f89034573163b91a473b349d652848b4919203a6
Waqas Ahmed <wa02263@gmail.com>
2023-10-16 09:49:06 -0700

    stopwatch timer added

--- lib/view_models/stopwatch_viewmodel.dart
+++ lib/view_models/stopwatch_viewmodel.dart
@@ -1,3 +1,45 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class StopWatchViewModel extends ChangeNotifier {}
+class StopWatchViewModel extends ChangeNotifier {
+  final Stopwatch stopwatch = Stopwatch();
+  late Timer _timer;
+  bool isRunning = false;
+
+  StopWatchViewModel() {
+    _timer = Timer.periodic(const Duration(milliseconds: 100), _updateTime);
+  }
+
+  void _updateTime(Timer timer) {
+    if (isRunning) {
+      notifyListeners();
+    }
+  }
+
+  void startStopwatch() {
+    stopwatch.start();
+    isRunning = true;
+    notifyListeners();
+  }
+
+  void stopStopwatch() {
+    stopwatch.stop();
+    isRunning = false;
+    notifyListeners();
+  }
+
+  String formatTime() {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String minutes = twoDigits(stopwatch.elapsed.inMinutes.remainder(60));
+    String seconds = twoDigits(stopwatch.elapsed.inSeconds.remainder(60));
+    String milliseconds =
+        twoDigits(stopwatch.elapsed.inMilliseconds.remainder(1000) ~/ 10);
+    return '$minutes:$seconds:$milliseconds';
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+}
--- lib/views/stopwatch.dart
+++ lib/views/stopwatch.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wakey/view_models/stopwatch_viewmodel.dart';
 import 'package:wakey/widgets/stopwatch_button.dart';
 
 class StopWatchView extends StatefulWidget {
@@ -12,23 +14,40 @@ class StopWatchView extends StatefulWidget {
 class _StopWatchViewState extends State<StopWatchView> {
   @override
   Widget build(BuildContext context) {
+    StopWatchViewModel viewModel =
+        Provider.of<StopWatchViewModel>(context, listen: false);
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Consumer<StopWatchViewModel>(builder: (context, value, child) {
+              return Text(
+                value.formatTime(),
+                style: const TextStyle(
+                    fontFamily: "Roboto",
+                    fontSize: 48,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFFFFFFFF)),
+              );
+            }),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 stopwatchButton(
-                    onTap: () {},
+                    onTap: () {
+                      viewModel.startStopwatch();
+                    },
                     textColor: const Color(0xFF34344A),
                     context: context,
                     buttonTitle: "Start",
                     buttonColor: Theme.of(context).primaryColor),
                 stopwatchButton(
-                    onTap: () {},
+                    onTap: () {
+                      viewModel.stopStopwatch();
+                    },
                     textColor: const Color(0xFFFFFFFF),
                     context: context,
                     buttonTitle: "Lap",

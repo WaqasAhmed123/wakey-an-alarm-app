2bedd09d5521f9291bfcbf330882438345128f37
Waqas Ahmed <wa02263@gmail.com>
2023-10-26 09:31:32 -0700

    scroller added

--- lib/views/set_alarm.dart
+++ lib/views/set_alarm.dart
@@ -85,106 +85,23 @@ class _SetAlarmViewState extends State<SetAlarmView> {
             Container(
               // color: const Color(0xFF34344A),
               color: Theme.of(context).primaryColorDark,
-              height: MediaQuery.sizeOf(context).height * 0.4,
+              height: MediaQuery.sizeOf(context).height * 0.3,
               child: Column(children: [
                 Expanded(
-                    child: CupertinoPicker(
-                  itemExtent: 32,
-                  scrollController: FixedExtentScrollController(
-                      initialItem:
-                          3), // Initial middle element is 3 (0-based index)
-                  onSelectedItemChanged: (index) {
-                    setState(() {
-                      selectedHour = index +
-                          1; // Adjust the selectedHour value based on the initial index
-                    });
-                  },
-                  children: [
-                    ListView.builder(
-                        itemCount: 12,
-                        itemBuilder: (context, index) {
-                          int displayedValue = index + 1;
-                          bool isBold = index == 2;
-                          return Center(
-                            child: Text(
-                              '$displayedValue',
-                              style: TextStyle(
-                                color: isBold
-                                    ? Colors.white
-                                    : Colors
-                                        .grey, // Set color based on bold or regular
-                                fontWeight: isBold
-                                    ? FontWeight.bold
-                                    : FontWeight.normal, // Set bold or regular
-                              ),
-                            ),
-                          );
-                        })
-                  ],
-                  // children: List<Widget>.generate(5, (index) {
-                  //   int displayedValue = index + 1;
-                  //   bool isBold = index == 2; // Middle element is bold
-                  //   return Center(
-                  //     child: Text(
-                  //       '$displayedValue',
-                  //       style: TextStyle(
-                  //         color: isBold
-                  //             ? Colors.white
-                  //             : Colors
-                  //                 .grey, // Set color based on bold or regular
-                  //         fontWeight: isBold
-                  //             ? FontWeight.bold
-                  //             : FontWeight.normal, // Set bold or regular
-                  //       ),
-                  //     ),
-                  //   );
-                  // }),
-                ))
-              ])
-              // Alarm Time Selection
-              // Minutes Picker
-              // CupertinoPicker(
-              //   itemExtent: 32.0,
-              //   backgroundColor: Colors.grey[200],
-              //   onSelectedItemChanged: (index) {
-              //     setState(() {
-              //       selectedMinute = index;
-              //     });
-              //   },
-              //   children: List<Widget>.generate(60, (index) {
-              //     return Center(
-              //       child: Text(
-              //         index.toString().padLeft(2, '0'),
-              //         style: const TextStyle(color: Colors.black),
-              //       ),
-              //     );
-              //   }),
-              // ),
-              // // AM/PM Picker
-              // CupertinoPicker(
-              //   itemExtent: 32.0,
-              //   backgroundColor: Colors.grey[200],
-              //   onSelectedItemChanged: (index) {
-              //     setState(() {
-              //       isAm = index == 0;
-              //     });
-              //   },
-              //   children: const <Widget>[
-              //     Center(
-              //       child: Text(
-              //         'AM',
-              //         style: TextStyle(color: Colors.black),
-              //       ),
-              //     ),
-              //     Center(
-              //       child: Text(
-              //         'PM',
-              //         style: TextStyle(color: Colors.black),
-              //       ),
-              //     ),
-              //   ],
-              // ),
-              ,
+                    child: ListWheelScrollView.useDelegate(
+                        itemExtent: 40,
+                        childDelegate: ListWheelChildBuilderDelegate(
+                            childCount: 12,
+                            builder: (context, index) {
+                              int displayedValue = index + 1;
+                              return Center(
+                                child: Text(
+                                  '$displayedValue',
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              );
+                            })))
+              ]),
             ),
           ],
         ),

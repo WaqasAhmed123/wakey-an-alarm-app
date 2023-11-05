f46ed6f868d51c1fdf22f0f8dd05adefc7926583
Waqas Ahmed <wa02263@gmail.com>
2023-11-05 08:59:24 -0800

    options for alarm added

--- lib/view_models/set_alarm_viewmodel.dart
+++ lib/view_models/set_alarm_viewmodel.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 class SetAlarmViewModel extends ChangeNotifier {
+ bool toggleAlarm = false;
+  onToggle() {
+    toggleAlarm = !toggleAlarm;
+    notifyListeners();
+  }
+
   int? getSelectedHour;
   setSelectedHour({selectedHour}) {
     getSelectedHour = selectedHour;
--- lib/widgets/listtile_container.dart
+++ lib/widgets/listtile_container.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wakey/utils/text_style.dart';
+import 'package:wakey/view_models/set_alarm_viewmodel.dart';
 
-listtileContainer({leadingText, tralingText, onTap}) {
+import '../view_models/home_viewmodel.dart';
+
+listtileContainer({leadingText, tralingText, onTap, isToggleButton=false, context}) {
+  final setAlarmViewModel =
+      Provider.of<SetAlarmViewModel>(context, listen: false);
+
   return InkWell(
     onTap: onTap,
     child: ListTile(
-      leading: Text(leadingText,style: textStyle()["titleSmall"],),
-      trailing: Row(
-         mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(tralingText),
-          const Icon(Icons.keyboard_arrow_right_rounded)
-        ],
+      leading: Text(
+        leadingText,
+        style: textStyle()["titleSmall"],
       ),
+      trailing: isToggleButton
+          ? Consumer<SetAlarmViewModel>(builder: (context, value, child) {
+              return Switch(
+                  activeColor: const Color(0xFFFFFFFF),
+                  activeTrackColor: const Color(0xFFF0F757),
+                  inactiveThumbColor: Colors.blueGrey.shade600,
+                  inactiveTrackColor: Colors.grey.shade400,
+                  splashRadius: 50.0,
+                  value: value.toggleAlarm,
+                  // changes the state of the switch
+                  onChanged: (value) => setAlarmViewModel.onToggle());
+            })
+          : Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(tralingText),
+                const Icon(Icons.keyboard_arrow_right_rounded)
+              ],
+            ),
     ),
   );
 }

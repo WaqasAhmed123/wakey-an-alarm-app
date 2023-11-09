0f9f04b0f5987ffae2272826db5edcddac658e0c
Waqas Ahmed <wa02263@gmail.com>
2023-11-09 07:56:47 -0800

    options for alarm added

--- lib/view_models/set_alarm_viewmodel.dart
+++ lib/view_models/set_alarm_viewmodel.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
 class SetAlarmViewModel extends ChangeNotifier {
-  List<String> days = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
-  ];
-  List<bool> selectedDays = [false, false, false, false, false, false, false];
-  setSelectedDays({index, value}) {
-    selectedDays[index] = value!;
+  // Map<String, dynamic> alarmDetails = {};
+  Map<String, bool> alarmDays = {
+    'Monday': false,
+    'Tuesday': false,
+    'Wednesday': false,
+    'Thursday': false,
+    'Friday': false,
+    'Saturday': false,
+    'Sunday': false
+  };
+  // List<bool> selectedDays = [false, false, false, false, false, false, false];
+  setAlarmDays({index, value}) {
+    alarmDays[index] = value!;
     notifyListeners();
   }
 
--- lib/widgets/alarm_days.dart
+++ lib/widgets/alarm_days.dart
@@ -2,82 +2,82 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wakey/view_models/set_alarm_viewmodel.dart';
 
-List<String> days = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday'
-];
-List<bool> selectedDays = [false, false, false, false, false, false, false];
+// List<String> days = [
+//   'Monday',
+//   'Tuesday',
+//   'Wednesday',
+//   'Thursday',
+//   'Friday',
+//   'Saturday',
+//   'Sunday'
+// ];
+// List<bool> selectedDays = [false, false, false, false, false, false, false];
 alarmDays({context}) {
   final setAlarmViewModel =
       Provider.of<SetAlarmViewModel>(context, listen: false);
   return showDialog(
       context: context,
       builder: (context) {
-        return AlertDialog(
-          title: const Text('Repeat'),
-          content: SizedBox(
-            width: double.maxFinite,
-            height: MediaQuery.sizeOf(context).height * 0.35,
-            child: Column(
-              children: [
-                Expanded(
-                  child: StatefulBuilder(
-                    builder: (context, setState) {
-                      return Consumer<SetAlarmViewModel>(
-                          builder: (context, value, child) {
-                        return ListView.builder(
-                          itemCount: value.days.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return ListTile(
-                              title: Text(value.days[index]),
-                              // leading: const Icon(Icons.calendar_today),
-                              trailing: Checkbox(
-                                value: value.selectedDays[index],
-                                onChanged: (value) {
-                                  // setState(() {
-                                  setAlarmViewModel.selectedDays[index] =
-                                      value!;
-                                  // });
-                                },
-                              ),
-                            );
-                          },
-                        );
-                      });
+        return StatefulBuilder(builder: (context, setState) {
+          return AlertDialog(
+            title: const Text('Repeat'),
+            content: SizedBox(
+              width: double.maxFinite,
+              height: MediaQuery.sizeOf(context).height * 0.35,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Consumer<SetAlarmViewModel>(
+                        builder: (context, value, child) {
+                      return ListView.builder(
+                        itemCount: value.alarmDays.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          String day = value.alarmDays.keys.elementAt(index);
+                          return ListTile(
+                            title: Text(value.alarmDays.keys.elementAt(index)),
+                            // leading: const Icon(Icons.calendar_today),
+                            trailing: Checkbox(
+                              value: value.alarmDays.values.elementAt(index),
+                              onChanged: (value) {
+                                setState(() {
+                                  // setAlarmViewModel.alarmDays.keys.elementAt(index) =
+                                  //     value!;
+                                  setAlarmViewModel.alarmDays[day] = value!;
+                                });
+                              },
+                            ),
+                          );
+                        },
+                      );
+                    }),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              const Divider(thickness: 1, color: Colors.black),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    child: const Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
                     },
                   ),
-                ),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            const Divider(thickness: 1, color: Colors.black),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  child: const Text('Cancel'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                TextButton(
-                  child: const Text('Done'),
-                  onPressed: () {
-                    // Perform the desired action with selectedDays
-                    print('Selected Days: $selectedDays');
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
-            )
-          ],
-        );
+                  TextButton(
+                    child: const Text('Done'),
+                    onPressed: () {
+                      // Perform the desired action with selectedDays
+                      // print('Selected Days: ${setAlarmViewModel.selectedDays}');
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              )
+            ],
+          );
+        });
       });
 }
 

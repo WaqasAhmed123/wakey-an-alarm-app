a741c441d1419df54773b355aa658d1b8d7ea5ce
Waqas Ahmed <wa02263@gmail.com>
2024-02-05 06:19:50 -0800

    geocoding added

--- lib/views/select_location.dart
+++ lib/views/select_location.dart
@@ -1,72 +1,8 @@
-// // ignore_for_file: must_call_super
-
-// import 'dart:async';
-
-// import 'package:flutter/material.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'package:provider/provider.dart';
-// import 'package:wakey/services/location_service.dart';
-// import 'package:wakey/view_models/select_location_viewmodel.dart';
-
-// import '../models/user_model.dart';
-
-// // class SelectLocationView extends StatefulWidget {
-// //   const SelectLocationView({super.key});
-
-// //   @override
-// //   State<SelectLocationView> createState() => _SelectLocationViewState();
-// // }
-
-// class SelectLocationView extends StatefulWidget {
-//   const SelectLocationView({Key? key}) : super(key: key);
-
-//   @override
-//   _SelectLocationViewState createState() => _SelectLocationViewState();
-// }
-
-// class _SelectLocationViewState extends State<SelectLocationView> {
-//   // final Completer<GoogleMapController> _controllerCompleter = Completer();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final selectLocationViewModel =
-//         Provider.of<SelectLocationViewModel>(context, listen: false);
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         centerTitle: true,
-//         title: const Text('Select Location'),
-//       ),
-//       body: GoogleMap(
-//         // onMapCreated: (GoogleMapController controller) {
-//         //   selectLocationViewModel.controllerCompleter.complete(controller);
-//         // },
-//         // onCameraMove: (CameraPosition position) {
-//         //   selectLocationViewModel.currentLocation = position.target;
-//         // },
-//          onLongPress: (LatLng point) {
-//           // Handle the long-press event here
-//           print('Long Pressed: $point');
-
-//           // You can update your ViewModel or perform any other action with the LatLng
-//           // selectLocationViewModel.updateLocation(point);
-//         },
-//         myLocationButtonEnabled: true,
-//         myLocationEnabled: true,
-//         initialCameraPosition: CameraPosition(
-//           target:
-//               selectLocationViewModel.currentLocation ?? const LatLng(0.0, 0.0),
-//           zoom: 15.0,
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart'; // Add this import for geocoding
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:wakey/utils/text_style.dart';
 import 'package:wakey/view_models/select_location_viewmodel.dart';
 
 class SelectLocationView extends StatefulWidget {
@@ -86,8 +22,9 @@ class _SelectLocationViewState extends State<SelectLocationView> {
 
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         centerTitle: true,
-        title: const Text('Select Location'),
+        title: Text('Select Location', style: textStyle()["titleMedium"]),
       ),
       body: Stack(children: [
         Consumer<SelectLocationViewModel>(builder: (context, value, child) {
@@ -114,11 +51,13 @@ class _SelectLocationViewState extends State<SelectLocationView> {
                   point.latitude, point.longitude);
               Placemark place = placemarks[0];
 
-              // Show bottom sheet with location info
+              // ignore: use_build_context_synchronously
               showModalBottomSheet(
+                isScrollControlled: true,
                 context: context,
                 builder: (BuildContext context) {
                   return Container(
+                      width: MediaQuery.sizeOf(context).width * 1,
                       padding: const EdgeInsets.all(16.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -132,17 +71,18 @@ class _SelectLocationViewState extends State<SelectLocationView> {
                             ),
                           ),
                           const SizedBox(height: 8.0),
-                          Text('Address: ${place.name}'),
-                          Text('City: ${place.locality}'),
-                          Text('State: ${place.administrativeArea}'),
-                          Text('Country: ${place.country}'),
+                          Text(
+                              'Address: ${place.name}, ${place.locality} ${place.country} '),
+                          // Text(
+                          //     'City: ${place.locality}, Country: ${place.country}'),
+                          // Text('State: ${place.administrativeArea}'),
+                          // Text('Country: ${place.country}'),
                         ],
                       ));
                 },
               ).whenComplete(() {
                 selectLocationViewModel.removeTempMarker();
               });
-
             },
             myLocationButtonEnabled: true,
             myLocationEnabled: true,

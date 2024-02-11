06b544423890a8ab8700629d61ff7800f77403c9
Waqas Ahmed <wa02263@gmail.com>
2024-02-11 09:24:43 -0800

    sqflite added

--- lib/models/alarm_model.dart
+++ lib/models/alarm_model.dart
@@ -1,4 +1,10 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class AlarmModel {
   String? alarmTitle;
+  String? alarmTime;
   List<String> alarmDays = [];
+  bool? deleteAfterRinging;
+  bool? vibrateWhenRinging;
+  LatLng? alarmLocation;
 }

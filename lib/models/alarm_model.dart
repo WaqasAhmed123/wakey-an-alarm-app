673f4565a40fe635265b8dfb5176dadb7a188600
Waqas Ahmed <wa02263@gmail.com>
2024-02-16 22:44:29 -0800

    sqflite added

--- lib/models/alarm_model.dart
+++ lib/models/alarm_model.dart
@@ -10,9 +10,6 @@
 // }
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:path/path.dart';
-//gpt's
-import 'package:sqflite/sqflite.dart';
 
 class AlarmModel {
   int? id;
@@ -69,48 +66,3 @@ class AlarmModel {
   }
 }
 
-class DatabaseHelper {
-  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
-  static Database? _database;
-
-  DatabaseHelper._privateConstructor();
-
-  Future<Database> get database async {
-    if (_database != null) return _database!;
-    _database = await _initDatabase();
-    return _database!;
-  }
-
-  Future<Database> _initDatabase() async {
-    String path = join(await getDatabasesPath(), 'alarms.db');
-    return await openDatabase(path, version: 1, onCreate: _createDatabase);
-  }
-
-  Future<void> _createDatabase(Database db, int version) async {
-    await db.execute('''
-      CREATE TABLE alarms(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        alarmTitle TEXT,
-        alarmTime TEXT,
-        alarmDays TEXT,
-        deleteAfterRinging INTEGER,
-        vibrateWhenRinging INTEGER,
-        alarmLocationLat REAL,
-        alarmLocationLng REAL
-      )
-    ''');
-  }
-
-  Future<int> insertAlarm(AlarmModel alarm) async {
-    Database db = await instance.database;
-    return await db.insert('alarms', alarm.toMap());
-  }
-
-  Future<List<AlarmModel>> getAlarms() async {
-    Database db = await instance.database;
-    List<Map<String, dynamic>> maps = await db.query('alarms');
-    return List.generate(maps.length, (i) {
-      return AlarmModel.fromMap(maps[i]);
-    });
-  }
-}
---
+++ lib/services/database_service.dart
@@ -0,0 +1,50 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../models/alarm_model.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  static Database? _database;
+
+  DatabaseHelper._privateConstructor();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'alarms.db');
+    return await openDatabase(path, version: 1, onCreate: _createDatabase);
+  }
+
+  Future<void> _createDatabase(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE alarms(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        alarmTitle TEXT,
+        alarmTime TEXT,
+        alarmDays TEXT,
+        deleteAfterRinging INTEGER,
+        vibrateWhenRinging INTEGER,
+        alarmLocationLat REAL,
+        alarmLocationLng REAL
+      )
+    ''');
+  }
+
+  Future<int> insertAlarm(AlarmModel alarm) async {
+    Database db = await instance.database;
+    return await db.insert('alarms', alarm.toMap());
+  }
+
+  Future<List<AlarmModel>> getAlarms() async {
+    Database db = await instance.database;
+    List<Map<String, dynamic>> maps = await db.query('alarms');
+    return List.generate(maps.length, (i) {
+      return AlarmModel.fromMap(maps[i]);
+    });
+  }
+}
